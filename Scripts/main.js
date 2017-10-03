556df4ac308cec99929da586d1e3f68b87cf3c05
KLC <32318793+Sevcav@users.noreply.github.com>
2017-10-03 12:39:24 -0700

    Added Input and output text fields

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,3 +2,18 @@
 function test() {
     alert("hello world");
 }
+
+function readText() {
+    //Grab the input element from the document
+    let inputItem = document.getElementById("stuff");
+    //Grab the text from the input element
+    let theText = inputItem.value;
+    //Demonstrate the inout/output 
+    alert("value is " + theText);
+    let theTextUpper = theText.toUpperCase();
+    //inputItem.value = theTextUpper;
+    //Grab the output element from the document
+    let outputItem = document.getElementById("stuff2");
+    outputItem.value = theTextUpper;
+
+}
